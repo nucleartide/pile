df9657581c0a8f5bccc0f40c334cb62e9d4e67d8
Jason Tu <jasontu4@gmail.com>
2018-08-24 07:55:36 -0700

    update

---
+++ pico8-enums.ts
@@ -0,0 +1,23 @@
+enum col {
+  black,
+  dark_blue,
+  dark_purple,
+  dark_green,
+  brown,
+  dark_gray,
+  light_gray,
+  white,
+  red,
+  orange,
+  yellow,
+  green,
+  blue,
+  indigo,
+  pink,
+  peach,
+}
+
+enum palette {
+  draw,
+  screen,
+}
